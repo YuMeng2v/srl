e0d258b93585805ae53a07fd2d7a693b9662d2a5
YuMeng2v <442413768@qq.com>
2022-02-09 15:03:15 +0800

    29

--- src/Routes/ReportSRL/ReportSRL.js
+++ src/Routes/ReportSRL/ReportSRL.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer'
-import Menu from '../../components/Menu/Menu'
+import Footer from '../../components/Footer/Footer';
+import Menu from '../../components/Menu/Menu';
+import {Minder} from 'react-mind'
 export default class ReportSRL extends Component {
+  constructor(props){
+    super(props)
+  }
+  componentDidMount(){
+    //
+  }
   render() {
     return <div className='rsrl-main'>
         <Header/>
-
+        <div className='layout'>
+            
+        </div>
+        <Footer/>
     </div>;
   } 
 }
